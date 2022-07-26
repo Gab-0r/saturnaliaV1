48650aef06ea23ff8f09c15966658010bea6fe15
Juan Gabriel Orozco <jgabriel.orozco@udea.edu.co>
2022-07-26 16:28:59 -0500

    Commit inicial. Splash funcional.

---
+++ app/src/main/java/com/example/saturnaliav1/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.saturnaliav1.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saturnaliav1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/saturnaliav1/ui/splash/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.saturnaliav1.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saturnaliav1.R
+import com.example.saturnaliav1.databinding.ActivitySplashBinding
+import com.example.saturnaliav1.ui.main.MainActivity
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = splashBinding.root
+        setContentView(view)
+        //Temporaizador para retornar al ui.main Activity
+        val timer = Timer()
+        timer.schedule(
+            timerTask{
+                goToMainActivity()
+            }, 1000
+        )
+    }
+
+    private fun goToMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
