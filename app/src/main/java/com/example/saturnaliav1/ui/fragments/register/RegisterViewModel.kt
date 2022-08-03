50ffff1c96a8fbd8b03cdc08e28230f1422deb47
Juan Gabriel Orozco <jgabriel.orozco@udea.edu.co>
2022-08-03 18:24:35 -0500

    Se agregan las funciones de comprobación de campos vacíos y contraseñas iguals a RegisterViewModel.kt, pendiente vincular la clase con la vista.

--- app/src/main/java/com/example/saturnaliav1/ui/fragments/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/saturnaliav1/ui/fragments/register/RegisterViewModel.kt
@@ -1,7 +1,47 @@
 package com.example.saturnaliav1.ui.fragments.register
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RegisterViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var discoName: MutableLiveData<String> = MutableLiveData()
+    var discoNameDone: LiveData<String> = discoName
+
+    private var discoEmail: MutableLiveData<String> = MutableLiveData()
+    var discoEmailDone: LiveData<String> = discoEmail
+
+    private var discoPass: MutableLiveData<String> = MutableLiveData()
+    var discoPassDone: LiveData<String> = discoPass
+
+    private var discoPassConfirm: MutableLiveData<String> = MutableLiveData()
+    var discoPassConfirmDonoe: LiveData<String> = discoPassConfirm
+
+    private var isPassOk: MutableLiveData<Boolean> = MutableLiveData()
+    var isPassOkDone: LiveData<Boolean> = isPassOk
+
+    private var position = 0
+    private var emptyFieldAt: MutableLiveData<Int> = MutableLiveData()
+    val emptyFieldAtDone: LiveData<Int> = emptyFieldAt
+
+    fun areEmptyFields(name_field: Boolean, lastname_field: Boolean,
+                       email_field: Boolean,  pass_field: Boolean, confpass_field: Boolean){
+        var fields = booleanArrayOf(name_field, lastname_field, email_field, pass_field, confpass_field)
+        for (i in fields.indices) {
+            Log.d("Verificando campo: ", i.toString() + fields.get(i).toString() )
+            if (fields.get(i)) {
+                position = i + 1
+                Log.d("Campo vacío en: ", position.toString())
+                emptyFieldAt.value = position
+                break
+            }
+        }
+    }
+
+    fun storeFields(discoName_: String, discoEmail_: String, discoPass_: String){
+        discoName.value = discoName_
+        discoEmail.value = discoEmail_
+        discoPass.value = discoPass_
+    }
 }
--- app/src/main/java/com/example/saturnaliav1/ui/fragments/register/registerFragment.kt
+++ app/src/main/java/com/example/saturnaliav1/ui/fragments/register/registerFragment.kt
@@ -6,27 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.saturnaliav1.R
+import com.example.saturnaliav1.databinding.FragmentRegisterBinding
 
 class registerFragment : Fragment() {
 
     companion object {
         fun newInstance() = registerFragment()
+        private lateinit var viewModel: RegisterViewModel
+        private lateinit var registerBinding: FragmentRegisterBinding
     }
-
-    private lateinit var viewModel: RegisterViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        registerBinding = FragmentRegisterBinding.inflate(inflater, container, false)
+        val view = registerBinding.root
+        return view
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
